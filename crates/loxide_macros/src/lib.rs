7cb98ec043d37c4759439fbb891bb4de343d9610
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-08-09 15:17:29 +0800

    Refactor: split some macros from `loxide_parser`

--- crates/loxide_macros/src/lib.rs
+++ crates/loxide_macros/src/lib.rs
@@ -1,6 +1,5 @@
-#[doc(hidden)]
 #[macro_export]
-/// the source file is relative to cargo manifest dir
+/// get the source file (relative to cargo manifest dir)
 macro_rules! src {
     ($($segment:expr),+) => {{
         let mut root: ::std::path::PathBuf = ::std::env!("CARGO_MANIFEST_DIR").into();
@@ -8,13 +7,4 @@ macro_rules! src {
         dbg!(&root);
         ::std::fs::read_to_string(root).unwrap()
     }}
-}
-
-#[doc(hidden)]
-#[macro_export]
-macro_rules! tokens {
-    ($src:expr) => {{
-        let scanner = $crate::scanner::Scanner::from($src);
-        scanner.collect::<::std::vec::Vec<_>>()
-    }};
 }
