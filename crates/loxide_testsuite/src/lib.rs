97b8e6560811eb75f473413fdb9fa8b95929a032
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2023-08-21 17:00:25 +0800

    Refactor: move `private-api` to `macro-support` (#27)

--- crates/loxide_testsuite/src/lib.rs
+++ crates/loxide_testsuite/src/lib.rs
@@ -1,9 +1,9 @@
-pub mod __private_api;
 mod macros;
 mod probe;
 mod unittest;
 
 #[doc(hidden)]
 pub mod _macro_support {
+    pub use crate::probe::Probe;
     pub use crate::unittest::tester::source_exec;
 }
