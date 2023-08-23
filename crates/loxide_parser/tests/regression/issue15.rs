127778d954aac9b8025e82a3192c7cb7d599a7fc
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-08-23 15:22:23 +0800

    Refactor: Migrate regression test using the unittest! API

--- crates/loxide_parser/tests/regression/issue15.rs
+++ crates/loxide_parser/tests/regression/issue15.rs
@@ -1,11 +1,5 @@
-use loxide_macros::src;
-
-#[test]
-fn issue15() {
-    regression!("issue15.lox", |path| {
-        let src = src!(path);
-        let mut parser = ::loxide_parser::parser::Parser::new(&src);
-        let result = parser.parse();
-        insta::assert_debug_snapshot!(result)
-    });
-}
+regression!(issue15, |src| {
+    let mut parser = ::loxide_parser::parser::Parser::new(src);
+    let result = parser.parse();
+    insta::assert_debug_snapshot!(result)
+});
--- crates/loxide_parser/tests/regression/issue18.rs
+++ crates/loxide_parser/tests/regression/issue18.rs
@@ -1,11 +1,5 @@
-use loxide_macros::src;
-
-#[test]
-fn issue18() {
-    regression!("issue18.lox", |path| {
-        let src = src!(path);
-        let mut parser = ::loxide_parser::parser::Parser::new(&src);
-        let result = parser.parse();
-        insta::assert_debug_snapshot!(result)
-    });
-}
+regression!(issue18, |src| {
+    let mut parser = ::loxide_parser::parser::Parser::new(src);
+    let result = parser.parse();
+    insta::assert_debug_snapshot!(result)
+});
