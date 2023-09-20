c455747dc944c6ba88ed12738080137221d92f22
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-09-20 16:38:21 +0800

    feat: interpreter draft API

---
+++ crates/loxide_interpreter/src/interpreter.rs
@@ -0,0 +1,20 @@
+use crate::environment::Environment;
+use crate::error::RuntimeError;
+use crate::eval::Evaluable;
+use crate::value::Value;
+
+pub struct Interpreter {
+    global: Environment,
+}
+
+impl Interpreter {
+    pub fn new() -> Self {
+        let global = Environment::global();
+
+        Interpreter { global }
+    }
+
+    pub fn interpret<E: Evaluable>(&mut self, executable: E) -> Result<Value, RuntimeError> {
+        executable.eval(&mut self.global)
+    }
+}
