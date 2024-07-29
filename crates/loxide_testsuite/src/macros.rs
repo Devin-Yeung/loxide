58bbcba6760eb82c190da9a7bd7e1e9d1bd49213
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-07-29 23:07:05 +0800

    doc: explain the `register!()`

--- crates/loxide_testsuite/src/macros.rs
+++ crates/loxide_testsuite/src/macros.rs
@@ -15,6 +15,18 @@ macro_rules! _function_name {
 }
 
 #[macro_export]
+/// caveat: `register!` highly depend on the function name
+/// ```
+/// # use loxide_testsuite::{footprints, register};
+/// fn share() {
+///     register!();
+///     footprints!();
+/// }
+///
+/// fn test01() { share(); }
+/// fn test02() { share(); }
+/// // they will share the same footprints
+/// ```
 macro_rules! register {
     () => {
         $crate::_macro_support::Probe::register($crate::_function_name!());
